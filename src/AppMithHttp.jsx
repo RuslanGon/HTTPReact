293e701ae39548798cfb01b4fac1e5b20f49650a
RuslanGon <ruslan441006@gmail.com>
2024-04-28 17:26:13 +0300

    wer

--- src/AppMithHttp.jsx
+++ src/AppMithHttp.jsx
@@ -3,41 +3,24 @@ import "./App.css";
 import axios from "axios";
 import Loader from "./component/Loader/Loader";
 
-// {
-//     "products": [
-//       {
-//         "id": 1,
-//         "title": "iPhone 9",
-//         "description": "An apple mobile which is nothing like apple",
-//         "price": 549,
-//         "discountPercentage": 12.96,
-//         "rating": 4.69,
-//         "stock": 94,
-//         "brand": "Apple",
-//         "category": "smartphones",
-//         "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-//         "images": [
-//           "https://cdn.dummyjson.com/product-images/1/1.jpg",
-//           "https://cdn.dummyjson.com/product-images/1/2.jpg",
-//           "https://cdn.dummyjson.com/product-images/1/3.jpg",
-//           "https://cdn.dummyjson.com/product-images/1/4.jpg",
-//           "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
-//         ]
-//       }
 
 const AppMithHttp = () => {
   const [products, setProducts] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchProducts() {
-      setIsLoading(true)
-      const { data } = await axios.get("https://dummyjson.com/products");
-      setProducts(data.products);
-      setIsLoading(false)
-
-      console.log(data);
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get("https://dummyjson.com/products");
+        setProducts(data.products);
+        console.log(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchProducts();
   }, []);
@@ -45,20 +28,22 @@ const AppMithHttp = () => {
   return (
     <div>
       <h1>Smart Ukrainian Big Product Store</h1>
-      {isLoading && <Loader /> }
+      {isLoading && <Loader />}
+      {isError && <p>Oops, something went wrong</p>}
       <ul>
-        {Array.isArray(products) && products.map(product => {
-          return (
-            <li key={product.id}>
-          <img width={250} src={product.thumbnail} alt={product.title} />
-          <h2>title: {product.title}</h2>
-          <h3>brand: {product.brand}</h3>
-          <p>description: {product.description}</p>
-          <h4>price: {product.price}</h4>
-          <p>rating: {product.rating}</p>
-        </li>
-          )
-        }) }
+        {Array.isArray(products) &&
+          products.map((product) => {
+            return (
+              <li key={product.id}>
+                <img width={250} src={product.thumbnail} alt={product.title} />
+                <h2>title: {product.title}</h2>
+                <h3>brand: {product.brand}</h3>
+                <p>description: {product.description}</p>
+                <h4>price: {product.price}</h4>
+                <p>rating: {product.rating}</p>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
